f47753b0c2e383bcdc4eaaa584ef3288446eadae
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-05-13 21:52:00 +0100

    more eslint rules added, need to be discussed

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,60 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    plugins: {
+      import: await import("eslint-plugin-import"),
+    },
+    rules: {
+      "import/order": [
+        "warn",
+        {
+          groups: [["builtin", "external"], "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      react: await import("eslint-plugin-react"),
+      "react-hooks": await import("eslint-plugin-react-hooks"),
+    },
+    rules: {
+      "react/self-closing-comp": "warn",
+      "react/jsx-sort-props": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    plugins: {
+      tailwindcss: await import("eslint-plugin-tailwindcss"),
+    },
+    rules: {
+      "tailwindcss/classnames-order": "warn",
+    },
+  },
+  {
+    plugins: {
+      jsxA11y: await import("eslint-plugin-jsx-a11y"),
+    },
+    rules: {
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/anchor-is-valid": "warn",
+      "jsx-a11y/no-autofocus": "warn",
+    },
+  },
+  {
+    rules: {
+      quotes: ["warn", "double", { avoidEscape: true }],
+      semi: ["warn", "always"],
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
 ];
 
 export default eslintConfig;
