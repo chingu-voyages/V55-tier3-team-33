49bb0d502fed140621b6ff4922743e4284e0fc0d
Yuri <yuri.oda.lily@gmail.com>
2025-05-23 04:29:19 +0900

    feat: Add user to db
    Co-authored-by: snowbytes <84378360+snowbytes@users.noreply.github.com>

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { createJWT } from '../utils/jwt.js';
+import { insert } from '../db/db.js';
 
 // @types/bcrypt @types/express -D
 type ValidationErrors = {
@@ -41,6 +42,11 @@ export const register = async (req: Request, res: Response): Promise<void> => {
 
   // save in the db
   // TODO: IMPLEMENT THIS
+  const isStored = await insert({ email, password: hashedPassword }, 'Users');
+  if (!isStored) {
+    res.status(500).send();
+    return;
+  }
 
   // login flow
   /// create a JWT token
@@ -50,3 +56,44 @@ export const register = async (req: Request, res: Response): Promise<void> => {
   // res.json({ message: 'Hello from your Node.js backend!' });
   res.json({ accessToken });
 };
+
+export async function login(req: Request, res: Response): Promise<void> {
+  const { email, password } = req.body;
+
+  {
+    const errors: ValidationErrors = {};
+    // validate that everything is correct
+    const regex = /^[A-Z0-9._-]{3,64}@[A-Z0-9-]{3,64}\.[A-Z]{2,32}$/gi;
+    const isEmailValid = regex.test(email);
+    if (!isEmailValid) {
+      errors.email = 'email is malformed.';
+    }
+
+    const isPasswordValid = password.length >= 8;
+    if (!isPasswordValid) {
+      errors.password = 'password should be more than 8 characters';
+    }
+
+    if (Object.keys(errors).length > 0) {
+      res.status(400).json({ errors });
+      return;
+    }
+  }
+
+  // verify the credentials
+  // find the user using email
+  // then hash the password
+  const hashedPassword = await bcrypt.hash(password, 12);
+  // then compare them
+  // if (user.password != hashedPassword) {
+  //   res.status(401).json({ message: 'invalid email or password' })
+  //   return
+  // }
+
+  // create and return the jwt
+  // const accessToken = createJWT({ user.role });
+
+  /// send it back
+  // res.json({ message: 'Hello from your Node.js backend!' });
+  // res.json({ accessToken });
+}
