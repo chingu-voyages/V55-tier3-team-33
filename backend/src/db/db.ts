2b4558689fb9f31d9ead81fe32093edef975c39d
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-05-28 13:25:24 +0100

    feat(backend): create a module for basic db operations

---
+++ backend/src/db/db.ts
@@ -0,0 +1,35 @@
+import { createPool } from 'mysql2/promise';
+import { createId as createCuid, isCuid } from '@paralleldrive/cuid2';
+
+import type { Pool } from 'mysql2/promise';
+let pool: Pool;
+
+export default { isValidId, makeDb, makeId };
+
+// making it asynchronous to mimick all the other db operations
+export async function makeDb(): Promise<Pool> {
+  if (!process.env.DB_URI) {
+    throw new Error('DB_URI not set. Please check your env vars');
+  }
+
+  if (!pool) {
+    try {
+      console.info('creating a connection pool for the database...');
+      pool = createPool(process.env.DB_URI);
+    } catch (err) {
+      console.error('pool creation failed.');
+      throw err;
+    }
+  }
+
+  console.info('db connection established');
+  return Promise.resolve(pool);
+}
+
+export function makeId(): string {
+  return createCuid();
+}
+
+export function isValidId(id: string): boolean {
+  return isCuid(id);
+}
