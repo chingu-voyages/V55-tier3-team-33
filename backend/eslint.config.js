072f49ca9204652c2b71c2f6cb6f0988bdff9345
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-05-28 11:15:43 +0100

    chore(backend): add node specific eslint plugin and tweak a couple lint rules

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,6 +1,7 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
 import ts from 'typescript-eslint';
-import prettierConfig from "eslint-config-prettier";
+import node from 'eslint-plugin-n';
+import prettierConfig from 'eslint-config-prettier';
 
 /**
  * @type {Array<import("eslint").Linter.Config>}
@@ -8,12 +9,17 @@ import prettierConfig from "eslint-config-prettier";
 export default [
   js.configs.recommended,
   ...ts.configs.recommended,
+  node.configs['flat/recommended-module'],
   prettierConfig,
   {
-    name: "own/recommended",
-    files: ["**/*.ts"],
+    name: 'own/recommended',
+    files: ['**/*.ts'],
     rules: {
-      '@typescript-eslint/no-unused-vars': ['warn'],
+      'no-console': ['error', { allow: ['error', 'warn', 'info'] }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { args: 'after-used', destructuredArrayIgnorePattern: '^_' },
+      ],
     },
   },
-]
+];
