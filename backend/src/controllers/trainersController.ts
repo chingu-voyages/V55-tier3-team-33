e7abf4bf97e6fd4d14fa203c41ab29bbbf8e7756
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-05-29 16:16:06 +0100

    setup trainers controller

--- backend/src/controllers/trainersController.ts
+++ backend/src/controllers/trainersController.ts
@@ -1,5 +1,12 @@
 import { type Request, type Response } from 'express';
+import { getTrainers } from '../services/trainersService.js';
 
-export const trainers = (req: Request, res: Response): void => {
-    res.json([{ id: '1', name: 'Ash Ketchum' }]);
+export const trainers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const trainers = await getTrainers();
+    res.status(200).json(trainers);
+  } catch (error) {
+    console.error('Error fetching trainers:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
