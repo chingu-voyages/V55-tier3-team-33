33147606dcdacb4427a9f7473c76488a48e20d16
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-05-30 12:03:57 +0100

    feat(db): add sql file to create db tables

---
+++ backend/src/db/exec_sql.ts
@@ -0,0 +1,38 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { argv } from 'node:process';
+import { fileURLToPath, URL } from 'node:url';
+import { makeDb } from './db.js';
+
+const filename = argv[2];
+const SQL_FILES_DIRNAME = fileURLToPath(
+  new URL('./sql_scripts', import.meta.url)
+);
+
+if (filename !== 'init.sql') {
+  // @ts-expect-error: ts error type needs to update to include error.cause
+  throw new Error('no valid sql filename provided', {
+    cause: { arg: { expected: 'existing sql_file_name', actual: filename } },
+  });
+}
+
+await execSqlFile(filename);
+
+export async function execSqlFile(filename: string): Promise<void> {
+  const pool = await makeDb();
+
+  const contents = await fs.readFile(path.join(SQL_FILES_DIRNAME, filename), {
+    encoding: 'utf8',
+  });
+
+  let queries = contents.split(';').map((str) => str.trim());
+  // last array element will be empty if the last sql query ends with ;
+  if (!queries[queries.length - 1]) {
+    queries = queries.slice(0, -1);
+  }
+
+  console.info(`executing ${filename}'s queries...`);
+  for (const query of queries) {
+    await pool.query(query);
+  }
+}
