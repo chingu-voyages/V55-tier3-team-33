a74b75de8e6cb04c2d13e1473ea490041b9e2356
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-06-02 06:39:51 +0100

    fix(db): update schemas and seed to accurately represent trainers

--- backend/src/db/sql_scripts/init.sql
+++ backend/src/db/sql_scripts/init.sql
@@ -5,13 +5,19 @@ CREATE TABLE person (
 	id VARCHAR(24) NOT NULL,
 	given_name VARCHAR(50) NOT NULL,
 	surname VARCHAR(50) NOT NULL,
-  city VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL UNIQUE,
 	email_verified BOOLEAN NOT NULL DEFAULT FALSE,
 	phone VARCHAR(50) UNIQUE,
-	is_trainer BOOLEAN NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY key (id)
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE trainer (
+  person_id VARCHAR(24) NOT NULL,
+  expertise VARCHAR(50) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE,
+  PRIMARY KEY (person_id)
 );
 
 CREATE TABLE workout (
@@ -25,7 +31,7 @@ CREATE TABLE workout (
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (client_id) REFERENCES person(id),
-  FOREIGN KEY (trainer_id) REFERENCES person(id),
+  FOREIGN KEY (trainer_id) REFERENCES trainer(person_id),
   PRIMARY KEY (id)
 );
 
@@ -35,12 +41,12 @@ CREATE TABLE discipline (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE person_discipline (
-	person_id VARCHAR(24) NOT NULL,
+CREATE TABLE trainer_discipline (
+	trainer_id VARCHAR(24) NOT NULL,
 	discipline_id SMALLINT NOT NULL,
-	FOREIGN KEY (person_id) REFERENCES person(id),
+	FOREIGN KEY (trainer_id) REFERENCES trainer(person_id),
 	FOREIGN KEY (discipline_id) REFERENCES discipline(id),
-	PRIMARY KEY (person_id, discipline_id)
+	PRIMARY KEY (trainer_id, discipline_id)
 );
 
 CREATE TABLE lang (
@@ -49,10 +55,10 @@ CREATE TABLE lang (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE person_lang (
-  person_id VARCHAR(24) NOT NULL,
+CREATE TABLE trainer_lang (
+  trainer_id VARCHAR(24) NOT NULL,
   lang_id SMALLINT NOT NULL,
-  FOREIGN KEY (person_id) REFERENCES person(id),
+  FOREIGN KEY (trainer_id) REFERENCES trainer(person_id),
   FOREIGN KEY (lang_id) REFERENCES lang(id),
-  PRIMARY KEY (person_id, lang_id)
+  PRIMARY KEY (trainer_id, lang_id)
 );
