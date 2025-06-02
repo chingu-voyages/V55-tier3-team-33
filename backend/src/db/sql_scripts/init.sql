3197c94b8ca4508aee5c722b2a458954b73dacc9
snowbytes <84378360+snowbytes@users.noreply.github.com>
2025-06-02 06:39:51 +0100

    fix(db): update schemas and seed to accurately represent trainers

--- backend/src/db/sql_scripts/init.sql
+++ backend/src/db/sql_scripts/init.sql
@@ -3,36 +3,21 @@
 -- see https://dev.mysql.com/doc/refman/8.4/en/implicit-commit.html
 CREATE TABLE person (
 	id VARCHAR(24) NOT NULL,
+	email VARCHAR(50) NOT NULL UNIQUE,
+  pwd VARCHAR(50) NOT NULL,
 	given_name VARCHAR(50) NOT NULL,
 	surname VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL UNIQUE,
-	email_verified BOOLEAN NOT NULL DEFAULT FALSE,
 	phone VARCHAR(50) UNIQUE,
+	email_verified BOOLEAN NOT NULL DEFAULT FALSE,
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE trainer (
   person_id VARCHAR(24) NOT NULL,
-  expertise VARCHAR(50) NOT NULL,
   city VARCHAR(50) NOT NULL,
   FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE,
   PRIMARY KEY (person_id)
-);
-
-CREATE TABLE workout (
-  id INT AUTO_INCREMENT,
-  client_id VARCHAR(24) NOT NULL,
-  trainer_id VARCHAR(24) NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  description TEXT,
-  start_time DATETIME NOT NULL,
-  end_time DATETIME NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (client_id) REFERENCES person(id),
-  FOREIGN KEY (trainer_id) REFERENCES trainer(person_id),
-  PRIMARY KEY (id)
 );
 
 CREATE TABLE discipline (
@@ -61,4 +46,21 @@ CREATE TABLE trainer_lang (
   FOREIGN KEY (trainer_id) REFERENCES trainer(person_id),
   FOREIGN KEY (lang_id) REFERENCES lang(id),
   PRIMARY KEY (trainer_id, lang_id)
+);
+
+CREATE TABLE workout (
+  id INT AUTO_INCREMENT,
+  client_id VARCHAR(24) NOT NULL,
+  trainer_id VARCHAR(24) NOT NULL,
+  discipline_id SMALLINT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  details TEXT NOT NULL,
+  start_time DATETIME NOT NULL,
+  end_time DATETIME NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (client_id) REFERENCES person(id),
+  FOREIGN KEY (trainer_id) REFERENCES trainer(person_id),
+  FOREIGN KEY (discipline_id) REFERENCES discipline(id),
+  PRIMARY KEY (id)
 );
