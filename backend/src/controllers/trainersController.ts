e681f164643230d250fd9729e95255a5e34e0456
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-06-03 18:28:34 +0100

    set single trainer endpoint

--- backend/src/controllers/trainersController.ts
+++ backend/src/controllers/trainersController.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response } from 'express';
-import { getTrainers } from '../services/trainersService.js';
+import { getTrainers, getTrainerById } from '../services/trainersService.js';
 
 export const trainers = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -7,6 +7,25 @@ export const trainers = async (req: Request, res: Response): Promise<void> => {
     res.status(200).json(trainers);
   } catch (error) {
     console.error('Error fetching trainers:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const trainerById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const id = req.params.id;
+  try {
+    const trainer = await getTrainerById(req.params.id);
+    if (!trainer) {
+      res.status(404).json({ message: 'Trainer not found' });
+      return;
+    } else {
+      res.status(200).json(trainer);
+    }
+  } catch (error) {
+    console.error(`Error fetching trainer ${id}:`, error);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
--- backend/src/services/trainersService.ts
+++ backend/src/services/trainersService.ts
@@ -1,6 +1,7 @@
-import { Trainers } from '../models/trainerModel.js';
+import { Trainers, Trainer } from '../models/trainerModel.js';
 import { makeDb } from '../db/db.js';
 
+// TODO : add pagination, handle errors
 export const getTrainers = async (): Promise<Trainers> => {
   const db = await makeDb();
   const [rows] = await db.query(`
@@ -25,4 +26,36 @@ export const getTrainers = async (): Promise<Trainers> => {
   `);
 
   return rows as Trainers;
+};
+
+export const getTrainerById = async (id: string): Promise<Trainer> => {
+  const db = await makeDb();
+  const [rows] = await db.query(
+    `
+    SELECT 
+      p.id AS trainer_id,
+      p.given_name,
+      p.surname,
+      p.email,
+      p.phone,
+      p.email_verified,
+      p.created_at,
+      t.city,
+      GROUP_CONCAT(DISTINCT d.name ORDER BY d.name SEPARATOR ', ') AS disciplines,
+      GROUP_CONCAT(DISTINCT l.name ORDER BY l.name SEPARATOR ', ') AS languages
+    FROM trainer t
+    JOIN person p ON t.person_id = p.id
+    LEFT JOIN trainer_discipline td ON t.person_id = td.trainer_id
+    LEFT JOIN discipline d ON td.discipline_id = d.id
+    LEFT JOIN trainer_lang tl ON t.person_id = tl.trainer_id
+    LEFT JOIN lang l ON tl.lang_id = l.id
+    WHERE p.id = ?
+    GROUP BY p.id, p.given_name, p.surname, p.email, p.phone, p.email_verified, p.created_at, t.city
+    `,
+    [id]
+  );
+
+  // TODO : remove the any type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return (rows as any[])[0] as Trainer;
 };
