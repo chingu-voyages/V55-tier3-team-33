9e94382152ee7e053202f888eadb9071bf3a77ef
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-06-09 12:22:31 +0100

    remove load of poppin font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import "./globals.css";
 import "@styles/styles.scss";
 import "@lib/fontawesome";
 import NavBar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
 export const metadata: Metadata = {
   title: "Loem Ipsum",
   description: "Lorem ipsum dolor est amet",
