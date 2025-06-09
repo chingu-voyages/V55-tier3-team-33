6f07a3489800530d13db8fe78b83c5b1a4b78f41
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-06-09 12:01:45 +0100

    code formatted

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/footer";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-})
+});
 export const metadata: Metadata = {
   title: "Loem Ipsum",
   description: "Lorem ipsum dolor est amet",
@@ -20,13 +20,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   // background image must go only on homepage
   return (
     <html lang="en" className="">
-      <body
-        className="bg-homepage flex flex-col min-h-screen font-sans"
-      >
+      <body className="bg-homepage flex flex-col min-h-screen font-sans">
         <NavBar />
         <main className="flex-grow flex flex-col">{children}</main>
         <Footer />
