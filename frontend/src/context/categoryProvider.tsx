3d3f80e34c47db8b11bd0eac115881486340e3e8
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-06-10 11:18:05 +0100

    setup react context for discipline category filter

---
+++ frontend/src/context/categoryProvider.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useReducer } from "react"
+import { CategoryContext, categoryReducer, initialState } from "./categoryContext"
+
+export default function RecipeProvider({ children }: { children: React.ReactNode }) {
+    const [state, dispatch] = useReducer(categoryReducer, initialState)
+    return (
+        <CategoryContext.Provider value={{ state, dispatch }}>
+            {children}
+        </CategoryContext.Provider>
+    )
+}
