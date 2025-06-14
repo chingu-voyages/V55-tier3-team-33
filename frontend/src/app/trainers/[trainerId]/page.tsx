a8aaa1e3283bd42b9c95cd62a762af31f61903fe
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-06-14 11:53:30 +0100

    remove left conflict message

--- frontend/src/app/trainers/[trainerId]/page.tsx
+++ frontend/src/app/trainers/[trainerId]/page.tsx
@@ -1,10 +1,13 @@
 import { getTrainer } from "@/lib/api";
 
+export default async function TrainersPage({
+  params,
+}: {
+  params: { trainerId: string };
+}) {
+  const { trainerId } = await params;
+  const trainer = await getTrainer(trainerId);
 
-export default async function TrainersPage({ params }: { params: { trainerId: string } }) {
-    const { trainerId } = await params
-    const trainer = await getTrainer(trainerId);
-
-    return <div>TrainersPage: {JSON.stringify(trainer)}</div>;
-    // return <div>TrainersPage: {trainerId}</div>;
+  return <div>TrainersPage: {JSON.stringify(trainer)}</div>;
+  // return <div>TrainersPage: {trainerId}</div>;
 }
