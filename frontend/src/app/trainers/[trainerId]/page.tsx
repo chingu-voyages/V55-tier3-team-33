1ecc091fb652ef7d6479df5bc6f77b62f268d86a
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-06-14 14:07:33 +0100

    setup pages navigation between trainer/trainers

--- frontend/src/app/trainers/[trainerId]/page.tsx
+++ frontend/src/app/trainers/[trainerId]/page.tsx
@@ -1,9 +1,10 @@
 import { getTrainer } from "@/lib/api";
 import Image from "next/image";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 export default async function TrainersPage({
   params,
@@ -12,8 +13,6 @@ export default async function TrainersPage({
 }) {
   const { trainerId } = await params;
   const trainer = await getTrainer(trainerId);
-
-  console.log("trainer:", trainer);
 
   if (trainer === null) {
     return (
@@ -25,9 +24,12 @@ export default async function TrainersPage({
 
   return (
     <div className="container mx-auto px-4 py-8 max-w-[680px]">
-      <div className="mb-4 font-bold"><FontAwesomeIcon icon={faArrowAltCircleLeft} /> Back</div>
+      <Link href="/trainers">
+        <div className="mb-4 font-bold">
+          <FontAwesomeIcon icon={faArrowAltCircleLeft} /> Back
+        </div>
+      </Link>
       <Card className="flex flex-col items-center p-8 text-center">
-
         <h1 className="text-3xl font-bold mb-6">
           {trainer.given_name} {trainer.surname}
         </h1>
@@ -39,9 +41,7 @@ export default async function TrainersPage({
         />
 
         <CardContent>
-          <div className="flex gap-4 mb-2">
-            Experience: 150 years
-          </div>
+          <div className="flex gap-4 mb-2">Experience: 150 years</div>
           <div className="flex gap-2 mb-2">
             Disciplines:
             {trainer.disciplines.map((discipline) => (
@@ -54,9 +54,7 @@ export default async function TrainersPage({
               <span key={languages}>{languages}</span>
             ))}
           </div>
-          <div className="flex gap-4 mb-2">
-            City: {trainer.city}
-          </div>
+          <div className="flex gap-4 mb-2">City: {trainer.city}</div>
           <div className="flex text-left gap-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
             condimentum nisl at nisl ullamcorper, at placerat arcu efficitur.
@@ -68,7 +66,6 @@ export default async function TrainersPage({
             <Button>Book Session</Button>
           </div>
         </CardFooter>
-
       </Card>
     </div>
   );
